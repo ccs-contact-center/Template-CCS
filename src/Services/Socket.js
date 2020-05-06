a2b425b6c99bd61df1a8ffc91f49b4af505aef89
Isaac Contreras <iacontrerasg@icloud.com>
2020-05-06 14:26:31 -0500

    Update Socket.js

--- src/Services/Socket.js
+++ src/Services/Socket.js
@@ -3,4 +3,6 @@ import socketIOClient from "socket.io-client";
 const URL = "https://socket.ccscontactcenter.com";
 //const URL = "http://localhost:8082";
 
-export var socket = socketIOClient(URL);
+export var socket = socketIOClient(URL, {
+  transports: ["websocket"],
+});
