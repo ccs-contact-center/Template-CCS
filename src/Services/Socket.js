0f0783c1cc2bd697d4a1f4f410424269e9837b31
Isaac Contreras <iacontrerasg@icloud.com>
2020-05-08 16:14:48 -0500

    Update Socket.js

--- src/Services/Socket.js
+++ src/Services/Socket.js
@@ -1,4 +1,4 @@
-//const URL = "wss://socket.ccscontactcenter.com";
-const URL = "ws://localhost:8082";
+const URL = "wss://socket.ccscontactcenter.com";
+//const URL = "ws://localhost:8082";
 
 export var ws = new WebSocket(URL);
